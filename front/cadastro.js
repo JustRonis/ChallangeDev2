1770b0806837a28528de05fd60b7b993082920da
Ronaldo <ronaldo.luizcp@gmail.com>
2023-04-19 09:37:32 -0300

    integração com back-end - ainda não finalizado

--- front/cadastro.js
+++ front/cadastro.js
@@ -3,6 +3,8 @@ const pixCampos = document.getElementById("pix-campos");
 const contaCorrenteCampos = document.getElementById("conta-corrente-campos");
 const pixRadio = document.getElementById("pix");
 const contaCorrenteRadio = document.getElementById("contaCorrente");
+const form = document.querySelector('form');
+const formData = new FormData(form);
 
 
 pixRadio.addEventListener("click", function() {
@@ -25,4 +27,19 @@ $(document).ready(function() {
     $('#telefone').mask('(00) 00000-0000');
   
     $('#cpf').mask('000.000.000-00', { reverse: true });
+  });
+
+
+  fetch('/api/endpoint', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    console.log("sucesso! enviado para o back")
+  })
+  .catch(error => {
+    console.log("Erro ao enviar para o back end")
   });
