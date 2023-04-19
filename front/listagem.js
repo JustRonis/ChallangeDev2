01cca7e2a41f88c90a91818885d5e7d51d862ed8
Ronaldo <ronaldo.luizcp@gmail.com>
2023-04-19 17:34:48 -0300

    endpoint get e renderização dinânica

--- front/listagem.js
+++ front/listagem.js
@@ -3,18 +3,26 @@ fetch('http://127.0.0.1:8080/api/cliente')
 .then(clientes => {
   clientes.forEach(cliente => {
     const html = `
-      <div class="card mb-3">
-        <div class="card-body">
-          <h5 class="card-title">${cliente.nome}</h5>
-          <p class="card-text">Telefone: ${cliente.telefone}</p>
-          <p class="card-text">CPF: ${cliente.cpf}</p>
-          <p class="card-text">ID do Pedido: ${cliente.idPedido}</p>
-          <p class="card-text">Chave PIX: ${cliente.chavePix || 'Não informada'}</p>
-          <p class="card-text">Conta: ${cliente.numeroConta || 'Não informado'}</p>
-          <p class="card-text">Agência: ${cliente.agencia || 'Não informada'}</p>
-          <p class="card-text">Banco: ${cliente.banco || 'Não informado'}</p>
+        <div class="accordion" id="accordionExample">
+            <div class="accordion-item m-2">
+                <h2 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        ${cliente.nome}
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <p>Telefone: ${cliente.telefone}</p>
+                        <p>CPF: ${cliente.cpf}</p>
+                        <p>ID do Pedido: ${cliente.idPedido}
+                        <p>Chave pix: ${cliente.chavePix || '✖️'}</p>
+                        <p>Numero conta:  ${cliente.numeroConta || '✖️'}</p>
+                        <p>Agencia: ${cliente.agencia || '✖️'}</p>
+                        <p>Banco: ${cliente.banco || '✖️'}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     `;
     document.getElementById('clientes-container').insertAdjacentHTML('beforeend', html);
   });
