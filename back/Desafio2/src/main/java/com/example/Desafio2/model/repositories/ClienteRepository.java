eb0f2611fe1d5f7bfe90fe6ff27fe9609f6f7249
Ronaldo <ronaldo.luizcp@gmail.com>
2023-04-20 00:04:52 -0300

    ajustando ClienteRepository

--- back/Desafio2/src/main/java/com/example/Desafio2/model/repositories/ClienteRepository.java
+++ back/Desafio2/src/main/java/com/example/Desafio2/model/repositories/ClienteRepository.java
@@ -5,23 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.Desafio2.model.entities.Cliente;
+  
+public interface ClienteRepository extends MongoRepository<Cliente, String> {
 
-// @Repository
-// public interface ClienteRepository {
-//     public interface UserRepository extends MongoRepository<Cliente, Integer> {
-    
-    //     }
-    
-    public interface ClienteRepository extends MongoRepository<Cliente, String> {
-        
-        @Autowired
-        public BCryptPasswordEncoder passwordEncoder= new BCryptPasswordEncoder();
-    
-    public void salvarCliente(Cliente novoCliente);
-   /* {
-        String senhaCriptografada = passwordEncoder.encode(novoCliente.getSenha());
-        novoCliente.setSenha(senhaCriptografada);
-        clienteRepository.save(novoCliente);    
-    }*/
-    
 }
