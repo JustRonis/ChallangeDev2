4eb6ce86b16c178348c63eab313c0a457557df94
Ronaldo <ronaldo.luizcp@gmail.com>
2023-04-20 09:12:31 -0300

    ajustando collpse bootstrap para expandir apenas o card clicado

--- front/listagem.js
+++ front/listagem.js
@@ -1,16 +1,16 @@
 fetch('http://127.0.0.1:8080/api/cliente')
 .then(response => response.json())
 .then(clientes => {
-  clientes.forEach(cliente => {
+  clientes.forEach((cliente, index) => {
     const html = `
         <div class="accordion" id="accordionExample">
             <div class="accordion-item m-2">
-                <h2 class="accordion-header">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <h2 class="accordion-header" id="heading${index}">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
                         ${cliente.nome}
                     </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div id="collapse${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
                         <p>Telefone: ${cliente.telefone}</p>
                         <p>CPF: ${cliente.cpf}</p>
@@ -30,3 +30,4 @@ fetch('http://127.0.0.1:8080/api/cliente')
 .catch(error => {
   console.error(error);
 });
+
